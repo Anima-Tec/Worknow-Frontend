00863c3e407111be44f63bf4b836b5cbbb81a7a7
Avril Botti <avril.emery@anima.edu.uy>
2025-09-22 22:39:25 +0000

    landig-login

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Login from "./pages/Login.jsx";
 import ChooseRole from "./pages/ChooseRole.jsx";
 import HomeCompany from "./pages/HomeCompany.jsx";
 import HomeUser from "./pages/HomeUser.jsx";
+import Landing from "./pages/Landing.jsx";
+import { useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <Routes>
+       <Route path="/landing" element={<Landing />} />
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/choose" element={<ChooseRole />} />
